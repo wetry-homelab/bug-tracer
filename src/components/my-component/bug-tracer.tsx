01ad9c08eee2add552de18ccb75e579cf057a567
David <davgilson@live.fr>
2021-04-08 22:54:32 +0200

    add success message

--- src/components/my-component/bug-tracer.tsx
+++ src/components/my-component/bug-tracer.tsx
@@ -23,6 +23,8 @@ export class BugTracer {
   @State() explanation: string = '';
   @State() screenShot: boolean = true;
   @State() console: boolean = true;
+  @State() isSuccess: boolean = false;
+  @State() sending: boolean = false;
 
   logs: any[] = [];
 
@@ -35,7 +37,6 @@ export class BugTracer {
       current_error.apply(null, Array.from(arguments));
     }
 
-    console.log(this.projectid, this.api);
   }
 
   takeScreenshot() {
@@ -58,6 +59,7 @@ export class BugTracer {
 
   closeChat() {
     this.open = false;
+    this.isSuccess = false;
   }
 
   explanationChane(e) {
@@ -72,8 +74,20 @@ export class BugTracer {
     this.console = !this.console;
   }
 
+  clear() {
+    this.logs = [];
+    this.explanation = '';
+  }
+
+  onSuccess() {
+    this.clear();
+    this.isSuccess = true;
+  }
+
   async submitForm() {
-    if (this.explanation.trim().length > 0) {
+    if (!this.sending && this.explanation.trim().length > 0) {
+      this.sending = true;
+      const that = this;
       const data = new FormData();
       data.append('explanation', this.explanation);
       if (this.screenShot) {
@@ -83,10 +97,20 @@ export class BugTracer {
       if (this.console) {
         data.append('console', JSON.stringify(this.logs));
       }
-      console.log(data);
       data.append('projectID', this.projectid);
       const request = new XMLHttpRequest();
       request.open("POST", this.api);
+      function reqSuccess () {
+        console.log('response', this.responseText);
+        that.onSuccess();
+        that.sending = false;
+      }
+      function reqFail () {
+        console.log('fails');
+        that.sending = false;
+      }
+      request.addEventListener("load", reqSuccess);
+      request.addEventListener("error", reqFail);
       request.send(data);
     }
   }
@@ -114,7 +138,7 @@ export class BugTracer {
             <div class="chat-logs">
               <p>Hello, do you have a bug to report to us?</p>
               <form>
-                <textarea id="chat-input" placeholder="Describe the bug..." maxlength="1000" onInput={(e) => this.explanationChane(e)}>{this.explanation}</textarea>
+                <textarea id="chat-input" placeholder="Describe the bug..." maxlength="1000" onInput={(e) => this.explanationChane(e)} value={this.explanation}></textarea>
                 <div class="actions">
                   <label class="pure-material-checkbox">
                     <input type="checkbox" checked={this.screenShot} onChange={() => this.toggleScreenshot()} />
@@ -128,11 +152,19 @@ export class BugTracer {
                       <span class="pure-material-checkbox-text">Send console</span>
                     </span>
                   </label>
-                  <button type="button" class="chat-submit" id="chat-submit" onClick={() => this.submitForm()}>
+                  <button type="button" class="chat-submit" id="chat-submit"
+                    onClick={() => this.submitForm()}
+                    disabled={this.sending}>
                     <i class="material-icons">send</i>
                   </button>
                 </div>
               </form>
+              { this.isSuccess ? (<div class="overlay">
+                <i class="material-icons success-flag">
+                  done
+                </i>
+                <span class="success-message">Thank's for your feedback</span>
+              </div>) : '' }
             </div>
           </div>
         </div>
--- src/components/my-component/readme.md
+++ src/components/my-component/readme.md
@@ -7,11 +7,10 @@
 
 ## Properties
 
-| Property | Attribute | Description     | Type     | Default     |
-| -------- | --------- | --------------- | -------- | ----------- |
-| `first`  | `first`   | The first name  | `string` | `undefined` |
-| `last`   | `last`    | The last name   | `string` | `undefined` |
-| `middle` | `middle`  | The middle name | `string` | `undefined` |
+| Property    | Attribute   | Description | Type     | Default     |
+| ----------- | ----------- | ----------- | -------- | ----------- |
+| `api`       | `api`       | Api URL     | `string` | `undefined` |
+| `projectid` | `projectid` | Project ID  | `string` | `undefined` |
 
 
 ----------------------------------------------
